d9cc06adf34bce529a5a6db01a634c5e8b9bc0d0
karbal abdelhadi <abdoraja2005@gmail.com>
2024-03-06 01:48:03 +0100

    task0 fix pycodestyle and documentation

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,22 +1,28 @@
+#!/usr/bin/python3
+"""  Python script that  queries the Reddit API
+and returns the number of subscribers  """
 import requests
 
+
 def number_of_subscribers(subreddit):
+    """ function that queries the Reddit API and returns the
+    number of subscribers """
     # Reddit API URL for fetching subreddit information
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    
+
     headers = {'User-Agent': 'CustomUserAgent'}
-    
+
     # the GET request to the Reddit API
     response = requests.get(url, headers=headers)
-    
+
     # Check if the request was successful (status code 200)
     if response.status_code == 200:
         # Parse the JSON response
         data = response.json()
-        
+
         # Extract the number of subscribers from the response
         subscribers_count = data['data']['subscribers']
-        
+
         return subscribers_count
     else:
         # Handle other error cases
